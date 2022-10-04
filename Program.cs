3919cd0f28cae60be3806f410a62566dbecfa10e
Eugene <80662982619q@gmail.com>
2022-10-04 08:06:41 +0300

    rerrrrrr

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello");
+Console.WriteLine("1111111111111Hello222222222222222");
 Console.WriteLine("Hello");Console.WriteLine("Hello");Console.WriteLine("Hello");
 var q=Console.ReadKey(true);
 
